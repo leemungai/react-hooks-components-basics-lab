f325d40dbe1fd63056ea59a9671d4416c1a19e4b
leemungai <mrmungai@yahoo.com>
2024-04-25 14:04:30 +0200

    Complete Components Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,27 @@
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
+import NavBar from "./NavBar";
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
+import Home from "./Home";
+
+
+
+
+function About(){
+  return(
+    <div id="about">
+      <h2> About </h2>
     </div>
   );
 }
-
-{/* write an <About> component here */}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
